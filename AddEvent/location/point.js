b4ac5d7eee3b68f847ae800819d009b8b413c729
Lost Elf <116311344+Chebil-Ilef@users.noreply.github.com>
2023-05-06 09:21:45 +0000

    Code fixes + links updates +code refractoring PART2
    Note: the front design of the shopping cart is made using JS (by Oumayma) for easier front design whereas in the backend part for practical reasons we're gonna use symfony php.

---
+++ AddEvent/location/point.js
@@ -0,0 +1,53 @@
+var pointJson = {
+    type: "FeatureCollection",
+    features: [
+        {
+            type: "Feature",
+            properties: {},
+            geometry: {
+                coordinates: [10.137940536595721, 36.80625307176682],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {},
+            geometry: {
+                coordinates: [10.137059952346476, 36.80892808233422],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {},
+            geometry: {
+                coordinates: [10.134573596817717, 36.810669899439134],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {},
+            geometry: {
+                coordinates: [10.131362054260165, 36.81019297726651],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {},
+            geometry: {
+                coordinates: [10.1311289584296, 36.808409676734556],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {},
+            geometry: {
+                coordinates: [10.134236902839802, 36.80722769884237],
+                type: "Point"
+            }
+        }
+    ]
+};
